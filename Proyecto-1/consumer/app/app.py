0d74e27984125f4b38d21774c95126ab168c5263
Miguel-arch <miguel7191@estudiantec.cr>
2022-10-19 22:11:14 -0600

    producer consumer

--- Proyecto-1/consumer/app/app.py
+++ Proyecto-1/consumer/app/app.py
@@ -7,4 +7,27 @@ from elasticsearch import Elasticsearch
 import hashlib
 import json
 
-# Buscar como publicar documentos
+def callback(ch, method, properties, body):
+    json_object = json.loads(body)
+    resp = client.index(index = ESINDEX, id = hashlib.md5(body).hexdigest(), document = json_object)
+    print(resp)
+    print(" [x] Received %r" % body)
+
+DATA = os.getenv('DATAFROMK8S')
+RABBIT_MQ = os.getenv('RABBITMQ')
+RABBIT_MQ_PASSWORD = os.getenv('RABBITPASS')
+QUEUE_NAME = os.getenv('RABBITQUEUE')
+ESENDPOINT = os.getenv('ESENDPOINT')
+ESPASSWORD = os.getenv('ESPASSWORD')
+ESINDEX = os.getenv('ESINDEX')
+print("aqui")
+client = Elasticsearch("https://" + ESENDPOINT + ":9200", basic_auth = ("elastic", ESPASSWORD), verify_certs = False)
+
+credentials = pika.PlainCredentials('user', RABBIT_MQ_PASSWORD)
+parameters = pika.ConnectionParameters(host = RABBIT_MQ, credentials = credentials)
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+channel.queue_declare(queue = QUEUE_NAME)
+channel.basic_consume(queue = QUEUE_NAME, on_message_callback = callback, auto_ack = True)
+print(' [*] Waiting for messages. TO exit press CTRL+C')
+channel.start_consuming()
