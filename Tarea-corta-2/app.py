a8354e315c1e68d525b451709611e4ad019be3c0
Dylan Stef Torres Walker <48809822+StefWalker@users.noreply.github.com>
2022-10-30 18:59:20 -0600

    API

--- Tarea-corta-2/app.py
+++ Tarea-corta-2/app.py
@@ -1,23 +1,59 @@
 from flask import Flask, jsonify, request
 from flask_ngrok import run_with_ngrok
 from flask_cors import CORS
-from data import data
+from data import markers
+from data import users
+from data import community
 
 app = Flask(__name__)
 CORS(app)
 
 run_with_ngrok(app)
     
+####
+# Main get request for each element in the json 
+# 
+
 @app.route('/', methods=["GET"])
 def getData():
-    return jsonify({"data": data})
+    return jsonify({"data": markers})
 
-@app.route('/data/<int:data_id>', methods=["GET"])
+@app.route('/markers', methods=["GET"])
+def getData():
+    return jsonify({"data": markers})
+
+@app.route('/users', methods=["GET"])
+def getData():
+    return jsonify({"data": users})
+
+@app.route('/community', methods=["GET"])
+def getData():
+    return jsonify({"data": community})
+
+####
+# get request by ID
+# 
+
+@app.route('/markers/<int:data_id>', methods=["GET"])
 def getDataId(data_id):
-    dato_encontrado = [dato for dato in data if dato['id'] == data_id]
+    dato_encontrado = [dato for dato in markers if dato['id'] == data_id]
     return jsonify({"dato": dato_encontrado})
 
-@app.route('/data', methods=['POST'])
+@app.route('/users/<int:data_id>', methods=["GET"])
+def getDataId(data_id):
+    dato_encontrado = [dato for dato in users if dato['id'] == data_id]
+    return jsonify({"dato": dato_encontrado})
+
+@app.route('/community/<int:data_id>', methods=["GET"])
+def getDataId(data_id):
+    dato_encontrado = [dato for dato in community if dato['id'] == data_id]
+    return jsonify({"dato": dato_encontrado})
+
+####
+# Set request by ID
+# 
+
+@app.route('/markers', methods=['POST'])
 def addData():
     new_data = {
         "id": request.json['id'],
@@ -31,9 +67,32 @@ def addData():
         "votos": request.json['votos'],
         "ultimaModificacion": request.json['ultimaModificacion'],
     }
-    data.append(new_data)
+    markers.append(new_data)
     return jsonify({"message": "Dato agregado",
-                    "dato": data})
+                    "dato": markers})
+
+@app.route('/users', methods=['POST'])
+def addData():
+    new_data = {
+        "id": request.json['id'],
+        "email": request.json['email'],
+        "friends": request.json['friends']
+    }
+    users.append(new_data)
+    return jsonify({"message": "Dato agregado",
+                    "dato": users})
+
+@app.route('/community', methods=['POST'])
+def addData():
+    new_data = {
+        "id": request.json['id'],
+        "name": request.json['name'],
+        "participantes": request.json['participantes'],
+        "messages": request.json['messages']
+    }
+    community.append(new_data)
+    return jsonify({"message": "Dato agregado",
+                    "dato": community})
 
 if __name__ == '__main__':
     app.run()
--- Tarea-corta-2/data.py
+++ Tarea-corta-2/data.py
@@ -1,4 +1,4 @@
-data = [
+markers = [
     {
         "id": 1,
         "latitud": 1.0043934,
@@ -7,21 +7,21 @@ data = [
         "fecha": "23/4/2022",
         "hora": "12:00",
         "mensaje": "Mensaje escrito by user",
-        "ubicacion": 1,
+        "ubicación": 1,
         "votos": 1,
-        "ultimaModificacion": "24/4/2022"
+        "ultimaModificación": "24/4/2022"
     },
     {
         "id": 2,
         "latitud": 1.0043934,
         "longitud": 4.43432,
         "peligro": 2,
-        "fecha": "24/4/2022",
+        "fecha": "23/4/2022",
         "hora": "12:00",
         "mensaje": "Mensaje escrito by user",
-        "ubicacion": 1,
+        "ubicación": 1,
         "votos": 1,
-        "ultimaModificacion": "24/4/2022"
+        "ultimaModificación": "24/4/2022"
     },
     {
         "id": 3,
@@ -31,8 +31,36 @@ data = [
         "fecha": "23/4/2022",
         "hora": "12:00",
         "mensaje": "Mensaje escrito by user",
-        "ubicacion": 1,
+        "ubicación": 1,
         "votos": 1,
-        "ultimaModificacion": "24/4/2022"
+        "ultimaModificación": "24/4/2022"
+    }
+]
+
+users = [
+    {
+        "id": 1,
+        "email": "1@fakemail.com",
+        "friends": ["@fakemail.com", "@fakemail.com", "@fakemail.com"]
+    },
+    {
+        "id": 2,
+        "email": "2@fakemail.com",
+        "friends": ["@fakemail.com", "@fakemail.com", "@fakemail.com"]
+    }
+]
+
+community = [
+    {
+        "id": 1,
+        "name": "Public",
+        "participantes": ["@fakemail.com", "@fakemail.com", "@fakemail.com"],
+        "messages": ["hola ", "mundo "]
+    },
+    {
+        "id": 2,
+        "name": "Public",
+        "participantes": ["@fakemail.com", "@fakemail.com", "@fakemail.com"],
+        "messages": ["hola ", "mundo "]
     }
 ]
