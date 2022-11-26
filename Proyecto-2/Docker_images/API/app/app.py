ae041234865453e790c028c502e0907d495f7588
Leemxch <61258635+Leemxch@users.noreply.github.com>
2022-11-26 00:20:08 -0600

    api comentado

--- Proyecto-2/Docker_images/API/app/app.py
+++ Proyecto-2/Docker_images/API/app/app.py
@@ -15,53 +15,74 @@ from flask import Flask, jsonify, request
 from flask_ngrok import run_with_ngrok
 from flask_cors import CORS
 
+# Instancia de la aplicación con flask, ngrok y cors
 app = Flask(__name__)
 #run_with_ngrok(app)
 CORS(app)
 
+# Se obtienen ingresan las credenciales de FireBase para usar la base de datos personal
 cred = credentials.Certificate('adminSecret.json')
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://tareacorta2-2f12e-default-rtdb.firebaseio.com/'
 })
 
+# Request básico para buscar los primeros 10 artículos
 @app.route('/search', methods=["POST"])
 def default():
+    # Offset de la siguiente página de la última visitada
     apiPage = 0
+    # Contador de artículos encontrados
     count = 0
+    # Lista para guardar los artículos
     articles= []
+    # Palabra a buscar en los títulos
     search = request.json["data"]
     while count < 1:
+        # Se accede al API de Bio 
         response = requests.get("https://api.biorxiv.org/covid19/" + str(apiPage)).json()
+        # Si el ciclo pasa del todal de artículos, se termina
         if apiPage == response["messages"][0]["total"]:
             break
+        # Agrega el artículo encontrado y summa el contador
         if re.search(search.lower(), response["collection"][0]["rel_title"].lower()) != None:
             articles += [response["collection"][0]]
             count += 1
+        # Cambio de página
         apiPage += 1
     response["collection"] = articles
     print(len(articles))
     response["messages"][0]["total"] = apiPage
     return response
 
+# Request para buscar otros 10 artículos relacionados
 @app.route('/more', methods=["POST"])
 def defaultMore():
+    # Offset de la siguiente página de la última visitada
     apiPage = int(request.json["offset"])
+    # Contador de artículos encontrados
     count = 0
+    # Lista para guardar los artículos
     articles= []
+    # Palabra a buscar en los títulos
     search = request.json["data"]
     while count < 1:
+        # Se accede al API de Bio 
         response = requests.get("https://api.biorxiv.org/covid19/" + str(apiPage)).json()
+        # Si el ciclo pasa del todal de artículos, se termina
         if apiPage == response["messages"][0]["total"]:
             break
+        # Agrega el artículo encontrado y summa el contador
         if re.search(search.lower(), response["collection"][0]["rel_title"].lower()) != None:
             articles += [response["collection"][0]]
             count += 1
+        # Cambio de página
         apiPage += 1
     response["collection"] = articles
     print(len(articles))
     response["messages"][0]["total"] = apiPage
     return response
 
+# Request para añadir un artículo con like del usuario a Firebase
 @app.route('/addLike/<data>', methods=["POST"])
 def addLike(data):
     # Global JSON path
@@ -69,17 +90,18 @@ def addLike(data):
     # Get all the data from the reference
     user = ref.get()
     try:
-        # Update the articles list
+        # Actualiza la lista de artículos
         ref.update({
             'articles': user["articles"] + [{"title": request.json['title'], "auth":request.json['auth'], "abs":request.json['abs']}]
         })
     except:
-        # Create a list of articles if they dont have one
+        # Crea una lista de artículos si no existe
         ref.update({
             'articles': [{"title": request.json['title'], "auth":request.json['auth'], "abs":request.json['abs']}]
         })
     return jsonify(ref.get())
 
+# Request para agregar un job a MariaDB
 @app.route('/addGrp/<data>', methods=["POST"])
 def addGrp(data):
     print(data)
