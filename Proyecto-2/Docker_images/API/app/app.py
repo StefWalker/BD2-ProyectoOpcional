42ebe586f18bd7c07e2411e15072d3adbbfaa406
Leemxch <61258635+Leemxch@users.noreply.github.com>
2022-11-26 23:23:30 -0600

    api finished

--- Proyecto-2/Docker_images/API/app/app.py
+++ Proyecto-2/Docker_images/API/app/app.py
@@ -22,59 +22,75 @@ firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://tareacorta2-2f12e-default-rtdb.firebaseio.com/'
 })
 
+# Variables de entorno
+ELASTICURL = os.getenv('ELASTICURL')
+ELASTICPASS = os.getenv('ELASTICPASS')
+ESINDEXGROUPS = os.getenv('ESINDEXGROUPS')
+
+# Conexion a Elasticsearch
+client = Elasticsearch("https://" + ELASTICURL + ":9200", basic_auth = ("elastic", ELASTICPASS), verify_certs = False)
+
+# Limpia los datos del indice
+def resetIndex():
+    if client.indices.exists(index = ESINDEXGROUPS):
+        client.indices.delete(index = ESINDEXGROUPS)
+        print("Indice groups eliminado")
+    client.indices.create(index = ESINDEXGROUPS)
+    print("Indice groups creado")
+
 # Request básico para buscar los primeros 10 artículos
-@app.route('/search', methods=["POST"])
-def default():
-    # Offset de la siguiente página de la última visitada
-    apiPage = 0
-    # Contador de artículos encontrados
+@app.route('/elastic', methods=["POST"])
+def search():
+    word = request.json["data"]
+    result = client.search(index = ESINDEXGROUPS, body = {"query":{"match_all":{}}})["hits"]["hits"]
+    docs = []
+    limit = 10
     count = 0
-    # Lista para guardar los artículos
-    articles= []
-    # Palabra a buscar en los títulos
-    search = request.json["data"]
-    while count < 10:
-        # Se accede al API de Bio 
-        response = requests.get("https://api.biorxiv.org/covid19/" + str(apiPage)).json()
-        # Si el ciclo pasa del todal de artículos, se termina
-        if apiPage == response["messages"][0]["total"]:
+    for search in result:
+        if count == limit:
             break
-        # Agrega el artículo encontrado y summa el contador
-        if re.search(search.lower(), response["collection"][0]["rel_title"].lower()) != None:
-            articles += [response["collection"][0]]
-            count += 1
-        # Cambio de página
-        apiPage += 1
-    response["collection"] = articles
-    response["messages"][0]["total"] = apiPage
-    return response
+        doc = search["_source"]["docs"]
+        for article in doc:
+            if count == limit:
+                break
+            collection = article["collection"]
+            for data in collection:
+                if count == limit:
+                    break
+                if re.search(word.lower(), data["rel_title"].lower()) != None:
+                    docs.append(data)
+                    count += 1
+                    print("Articulo agregado: " + data["rel_title"])
+    return jsonify(docs)
 
 # Request para buscar otros 10 artículos relacionados
-@app.route('/more', methods=["POST"])
-def defaultMore():
-    # Offset de la siguiente página de la última visitada
-    apiPage = int(request.json["offset"])
-    # Contador de artículos encontrados
+@app.route('/elasticMore', methods=["POST"])
+def searchMore():
+    word = request.json["data"]
+    last = request.json["offset"]
+    result = client.search(index = ESINDEXGROUPS, body = {"query":{"match_all":{}}})["hits"]["hits"]
+    docs = []
+    limit = 10
     count = 0
-    # Lista para guardar los artículos
-    articles= []
-    # Palabra a buscar en los títulos
-    search = request.json["data"]
-    while count < 10:
-        # Se accede al API de Bio 
-        response = requests.get("https://api.biorxiv.org/covid19/" + str(apiPage)).json()
-        # Si el ciclo pasa del todal de artículos, se termina
-        if apiPage == response["messages"][0]["total"]:
+    insertFlag = False
+    for search in result:
+        if count == limit:
             break
-        # Agrega el artículo encontrado y summa el contador
-        if re.search(search.lower(), response["collection"][0]["rel_title"].lower()) != None:
-            articles += [response["collection"][0]]
-            count += 1
-        # Cambio de página
-        apiPage += 1
-    response["collection"] = articles
-    response["messages"][0]["total"] = apiPage
-    return response
+        doc = search["_source"]["docs"]
+        for article in doc:
+            if count == limit:
+                break
+            collection = article["collection"]
+            for data in collection:
+                if count == limit:
+                    break
+                if re.search(word.lower(), data["rel_title"].lower()) != None and insertFlag:
+                    docs.append(data)
+                    count += 1
+                    print("Articulo agregado: " + data["rel_title"])
+                if data["rel_title"] == last:
+                    insertFlag = True
+    return jsonify(docs)
 
 # Request para añadir un artículo con like del usuario a Firebase
 @app.route('/addLike/<data>', methods=["POST"])
@@ -118,48 +134,6 @@ def addGrp(data):
     mariaDatabase.commit()
     mariaDatabase.close()
     return jsonify(data)
-
-# Variables de entorno
-ELASTICURL = os.getenv('ELASTICURL')
-ELASTICPASS = os.getenv('ELASTICPASS')
-ESINDEXGROUPS = os.getenv('ESINDEXGROUPS')
-
-# Conexion a Elasticsearch
-client = Elasticsearch("https://" + ELASTICURL + ":9200", basic_auth = ("elastic", ELASTICPASS), verify_certs = False)
-
-# Limpia los datos del indice
-def resetIndex():
-    if client.indices.exists(index = ESINDEXGROUPS):
-        client.indices.delete(index = ESINDEXGROUPS)
-        print("Indice groups eliminado")
-    client.indices.create(index = ESINDEXGROUPS)
-    print("Indice groups creado")
-
-# Busca si word esta en rel_title
-@app.route('/elastic', methods=["POST"])
-def search():
-    word = request.json["data"]
-    result = client.search(index = ESINDEXGROUPS, body = {"query":{"match_all":{}}})["hits"]["hits"]
-    docs = []
-    count = 0
-    for search in result:
-        if count == 10:
-            break
-        doc = search["_source"]["docs"]
-        for article in doc:
-            if count == 10:
-                break
-            collection = article["collection"]
-            for data in collection:
-                if count == 10:
-                    break
-                title = data["rel_title"]
-                if word in title:
-                    docs.append(article)
-                    count += 1
-                    print("Articulo agregado: " + title)
-    response = "{\"articles\":\"{" + str(docs) + "}\"}"
-    return jsonify(response)
 
 @app.route('/', methods=["GET"])
 def init():
